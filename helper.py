385c23aac6ef261eba832491a4f93403f1427c1e
Osezele Oghedegbe <ozonichi@gmail.com>
2024-10-08 13:49:12 +0100

    Update

--- helper.py
+++ helper.py
@@ -27,6 +27,9 @@ async def send_message(bot: AsyncTeleBot, chat_id: Union[int, str], text: str,
                        reply_parameters: Optional[types.ReplyParameters] = None,
                        link_preview_options: Optional[types.LinkPreviewOptions] = None,
                        business_connection_id: Optional[str] = None):
+    """
+    Handles message actions
+    """
     global global_chat_id
     global_chat_id = chat_id
     sent_message = await bot.send_message(chat_id, text, reply_markup=reply_markup,
@@ -41,14 +44,17 @@ async def send_message(bot: AsyncTeleBot, chat_id: Union[int, str], text: str,
 
 
 async def delete_messages(bot: AsyncTeleBot):
+    "Handles delete messages"
     await bot.delete_messages(global_chat_id, global_message_ids)
 
 
 def create_buttons(button_text, button_callback, url=None):
+    "Creates a Button on an InlineKeyboardButton"
     return types.InlineKeyboardButton(button_text, callback_data=button_callback, url=url)
 
 
 def wait_for_user_input(message):
+    "Changes the state of the bot when waiting for users input"
     return bot_state.get(message.chat.id) == 'waiting_for_user_input'
 
 
